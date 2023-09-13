a4c5101908ae2f7c05f5adb1e1cfe0868ba26b72
liezl76 <liezl1106@gmail.com>
2023-09-13 15:20:22 +0800

    Add Prep1 code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        Console.WriteLine("What is your last name? ");
+        string firstname = Console.ReadLine();
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {firstname}, {firstname} {lastname}.");
+
     }
 }
