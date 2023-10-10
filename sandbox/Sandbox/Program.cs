49a64baf91b107edb01948cff635e1e286aac09c
liezl76 <liezl1106@gmail.com>
2023-10-10 13:53:33 +0800

    learn how clear works

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,46 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Formats.Asn1;
-using System.Runtime.InteropServices;
-using System.Text;
 
-public class Entry
+class Program
 {
-    
-    public List<Entry> entries = new List<Entry>();
-
-    string[] _questions = {
-        "Who was the most interesting person I interacted with today?",
-        "Who was the best part of the day?",
-        "How did I see the hand of the Lord in my life today?",
-        "What was the strongest emotion I felt today?",
-        "If I had one thing that I could do over today, what would it be?"
-    };
-    
-    public string _entry;
-    public DateTime date = DateTime.Now;
-    
-    public void PromptQuestion(string questions)
-    {        
-        Random rnd = new Random();
-        int num_question = rnd.Next(0, 4);
-        string selected_question = _questions[num_question];
-        Console.WriteLine(selected_question);
-        string answer = Console.ReadLine();
-        entries.Add(new Entry(answer));
-    }
-    public Entry(string answer)
+    static void Main(string[] args)
     {
-         this._entry = answer;
-    }
+        // This will start by displaying "AAA" and waiting for the user to press the enter key
+        Console.WriteLine("AAA");
+        Console.ReadLine();
 
-    public DateTime getDateTime()
-    {
-       return this.date;
-    }
+        // This will clear the console
+        Console.Clear();
 
-    public string getEntry()
-    {
-        return this._entry;
+        // This will show "BBB" in the console where "AAA" used to be
+        Console.WriteLine("BBB");
     }
 }
