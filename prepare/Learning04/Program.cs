36b54356f5e78cfb8d6072daca5dc06520beb944
liezl76 <liezl1106@gmail.com>
2023-10-23 14:56:02 +0800

    edit Learning 04 code

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,12 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Assignment a1 = new Assignment("Juan Ramos", "Difference");
-        // Console.WriteLine(a1.GetSummary());
+        // create a base object assignment
+        Assignment a1 = new Assignment("Juan Ramos", "Difference");
+        Console.WriteLine(a1.GetSummary());
 
-        // MathAssignment a2 = new MathAssignment("Juan Ramos", "Difference", "7-12", "8-9");
-        // Console.WriteLine(a2.GetSummary());
-        // Console.WriteLine(a2.GetHomeworkList());
+        //create the derived class assignments
+        MathAssignment a2 = new MathAssignment("Juan Ramos", "Difference", "7-12", "8-9");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
 
         WritingAssignment a3 = new WritingAssignment("Juan Ramos", "Chinese Story", "The Life of Pi");
         Console.WriteLine(a3.GetSummary());
