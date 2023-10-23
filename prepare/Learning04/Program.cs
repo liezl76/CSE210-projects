361ccbbd6bc5c2b5199f36f05af12f635c464e17
liezl76 <liezl1106@gmail.com>
2023-10-23 14:51:16 +0800

    Code Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Assignment a1 = new Assignment("Juan Ramos", "Difference");
+        // Console.WriteLine(a1.GetSummary());
+
+        // MathAssignment a2 = new MathAssignment("Juan Ramos", "Difference", "7-12", "8-9");
+        // Console.WriteLine(a2.GetSummary());
+        // Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Juan Ramos", "Chinese Story", "The Life of Pi");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
