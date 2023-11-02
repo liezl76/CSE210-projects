1ff6e64e03339bb08278b78a48e23666e8367e4f
liezl76 <liezl1106@gmail.com>
2023-11-03 05:38:42 +0800

    edit code breating class

--- prove/Develop04/Breathing.cs
+++ prove/Develop04/Breathing.cs
@@ -4,7 +4,8 @@ public class BreathingActivity : Activity
 {
     private string _breathInMessage;
     private string _breathOutMessage;
-    public BreathingActivity(string activityName, string description, int duration, string breathInMessage, string breathOutMessage) : base(description, activityName, duration)
+    public BreathingActivity(string activityName, string description, int duration, string breathInMessage, string breathOutMessage) 
+    : base(description, activityName, duration)
     {
         _breathInMessage = breathInMessage;
         _breathOutMessage = breathOutMessage;
@@ -14,10 +15,8 @@ public class BreathingActivity : Activity
         // Prompt the user to input the duration in seconds
         Console.Write("Enter the duration: ");
         int durationInSeconds = Convert.ToInt32(Console.ReadLine());
-        
         // Calculate the duration in milliseconds
         int durationInMilliseconds = durationInSeconds * 1000;
-        
         // Get the start time of the loop
         DateTime startTime = DateTime.Now;
         
