b4fcc99a9faad32d22c4da61c59633dd89433190
liezl76 <liezl1106@gmail.com>
2023-11-03 16:45:53 +0800

    edit code

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -27,8 +27,7 @@ class Program
         30, "Breath in...", "Breath out...");
         Activity reflectionActivity = new ReflectionActivity("Reflection Activity", 
         "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.", 
-        30, new List<string> {"Think of a time when you stood up for someone else.", "Think of a time when you did something really difficult.", "Think of a time when you helped someone in need."}, 
-        new List<string> { "Why was this experience meaningful to you?", "Have you ever done anything like this before?", "How did you get started?", "How did you feel when it was complete?" });
+        10, prompt, questions);
         Activity listingActivity = new ListingActivity("Listing Activity", "List items", 90, prompts, items);
 
         bool exitProgram = false;
