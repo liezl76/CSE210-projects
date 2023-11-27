57d2b37c10fe527f3f6dd65af7348a95656d73f6
liezl76 <liezl1106@gmail.com>
2023-11-27 15:07:10 +0800

    add code

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -7,6 +7,16 @@ class Program
         List<Video> videos = new List<Video>();
 
         //Create videos and add comments
-        
+        Video video1 = new Video("G.I Jane", "Demi Moore", 9000);
+        video1.AddComment("Luis", "Nice Movie!");
+        video1.AddComment("Maria", "Very inspiring!");
+
+
+
+        //Display video details
+        foreach (var video in videos)
+        {
+            video.DisplayVideoDetails();
+        }
     }
 }
