a299cfc84a30ac0d89b6d9497e54af20f201da42
liezl76 <liezl1106@gmail.com>
2023-12-05 10:53:24 +0800

    edit

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         // Create activities
-        Running runningActivity = new Running(new DateTime(2022, 11, 3), 30, 3.0);
-        StationaryBicycle bicycleActivity = new StationaryBicycle(new DateTime(2022, 11, 5), 30, 20.0);
-        LapSwimming swimmingActivity = new LapSwimming(new DateTime(2022, 11, 10), 30, 10);
+        Running runningActivity = new Running(new DateTime(2023, 11, 6), 30, 3.0);
+        StationaryBicycle bicycleActivity = new StationaryBicycle(new DateTime(2023, 11, 8), 30, 20.0);
+        LapSwimming swimmingActivity = new LapSwimming(new DateTime(2023, 11, 10), 30, 10);
 
         // Display summary for each activity
         Console.WriteLine(runningActivity.GetSummary());
