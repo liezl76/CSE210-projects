cdcdd248d74d6f23115b534f1a04cd24d1f192bc
liezl76 <liezl1106@gmail.com>
2023-12-07 09:19:38 +0800

    edit foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -7,11 +7,13 @@ class Program
         // Create activities
         Running runningActivity = new Running(new DateTime(2023, 11, 6), 30, 3.0);
         StationaryBicycle bicycleActivity = new StationaryBicycle(new DateTime(2023, 11, 8), 30, 20.0);
-        LapSwimming swimmingActivity = new LapSwimming(new DateTime(2023, 11, 10), 30, 10);
+        LapSwimming swimmingActivity = new LapSwimming(new DateTime(2023, 11, 10), 240, 5);
 
         // Display summary for each activity
+        Console.WriteLine();
         Console.WriteLine(runningActivity.GetSummary());
         Console.WriteLine(bicycleActivity.GetSummary());
         Console.WriteLine(swimmingActivity.GetSummary());
+        Console.WriteLine();
     }
 }
